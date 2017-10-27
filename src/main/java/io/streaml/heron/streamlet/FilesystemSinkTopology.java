c2006ff1955d0b8906bb0dc44009194595fcec66
Luc Perkins <lucperkins@gmail.com>
2017-10-26 17:10:16 -0700

    fix file sink logic

--- src/main/java/io/streaml/heron/streamlet/FilesystemSinkTopology.java
+++ src/main/java/io/streaml/heron/streamlet/FilesystemSinkTopology.java
@@ -1,36 +1,57 @@
 package io.streaml.heron.streamlet;
 
+import com.twitter.heron.api.utils.Utils;
 import com.twitter.heron.dsl.*;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class FilesystemSinkTopology {
     private static class FilesystemSink<T> implements Sink<T> {
-        private PrintWriter writer;
+        private Path tempFilePath;
+        private File tempFile;
+
+        FilesystemSink(File f) {
+            this.tempFile = f;
+        }
 
         public void setup(Context context) {
+            this.tempFilePath = Paths.get(tempFile.toURI());
+        }
+
+        public void put(T element) {
+            byte[] bytes = String.format("%s\n", element.toString()).getBytes();
+
             try {
-                File f = File.createTempFile("test", ".tmp");
-                this.writer = new PrintWriter(new FileWriter(f));
+                Files.write(tempFilePath, bytes, StandardOpenOption.APPEND);
+                System.out.println(
+                        String.format("Wrote %s to %s", new String(bytes), tempFilePath.toAbsolutePath())
+                );
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
 
-        public void put(T element) {
-            writer.append(element.toString());
-            writer.flush();
-            writer.close();
+        public void cleanup() {
         }
-
-        public void cleanup() {}
     }
 
     public static void main(String[] args) throws IOException {
         Builder builder = Builder.createBuilder();
+        
+        File f = File.createTempFile("test", ".tmp");
+        System.out.println(String.format("Ready to write to file %s", f.getAbsolutePath()));
 
-        builder.newSource(() -> "word")
-                .toSink(new FilesystemSink<>());
+        builder.newSource(() -> {
+                    Utils.sleep(500);
+                    return ThreadLocalRandom.current().nextInt(100);
+                })
+                .toSink(new FilesystemSink<>(f));
 
         Config config = new Config();
 
