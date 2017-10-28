5004c997584eda3accc39d6203bc51d8aecdcf26
Luc Perkins <lucperkins@gmail.com>
2017-10-27 17:28:34 -0700

    add more substance to transforms topology

--- src/main/java/io/streaml/heron/streamlet/TransformsTopology.java
+++ src/main/java/io/streaml/heron/streamlet/TransformsTopology.java
@@ -8,32 +8,43 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Consumer;
 
 public class TransformsTopology {
+    private static class DoNothingTransformer<T> implements SerializableTransformer<T, T> {
+        public void setup(Context context) {}
+
+        public void transform(T in, Consumer<T> consumer) {
+            consumer.accept(in);
+        }
+
+        public void cleanup() {}
+    }
+
+    private static class IncrementTransformer implements SerializableTransformer<Integer, Integer> {
+        private int increment;
+
+        public IncrementTransformer(int increment) {
+            this.increment = increment;
+        }
+
+        public void setup(Context context) {
+        }
+
+        public void transform(Integer in, Consumer<Integer> consumer) {
+            int incrementedValue = in + increment;
+            consumer.accept(incrementedValue);
+        }
+
+        public void cleanup() {}
+    }
+
     public static void main(String[] args) throws Exception {
         Builder builder = Builder.createBuilder();
 
-        builder.newSource(() -> {
-                    Utils.sleep(100);
-                    int rand = ThreadLocalRandom.current().nextInt(100);
-                    System.out.println("Emitting random integer " + rand);
-                    return rand;
-                 })
-                .transform(new SerializableTransformer<Integer, Integer>() {
-                    private int count;
-
-                    public void setup(Context context) {
-                        count = (int) context.getState().getOrDefault("count", 0);
-                        System.out.println("Read state: " + count);
-                    }
-
-                    public void transform(Integer i, Consumer<Integer> consumer) {
-                        System.out.println("Incoming element: " + i);
-                        i = i + 50;
-                        consumer.accept(i);
-                        System.out.println("New state: " + i);
-                    }
-
-                    public void cleanup() {}
-                })
+        builder.newSource(() -> ThreadLocalRandom.current().nextInt(100))
+                .transform(new DoNothingTransformer<>())
+                .transform(new IncrementTransformer(10))
+                .transform(new IncrementTransformer(-7))
+                .transform(new DoNothingTransformer<>())
+                .transform(new IncrementTransformer(-3))
                 .log();
 
         String topologyName = StreamletUtils.getTopologyName(args);
